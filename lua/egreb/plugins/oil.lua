a0d1a9e4a8cea8ba3b3054a88c21cbad55da61bd
Simen Berge <bergesimen@gmail.com>
2024-07-17 16:55:06 +0200

    split init config into multiple files

---
+++ lua/egreb/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {},
+  -- Optional dependencies
+  dependencies = {},
+  config = function()
+    require('oil').setup {
+      default_file_explorer = true,
+      columns = {
+        'icon',
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+      float = {
+        padding = 2,
+        max_width = 80,
+        max_height = 40,
+        border = 'rounded',
+      },
+    }
+
+    -- [[ OIL ]]
+    vim.keymap.set('n', '<leader>b', ':Oil --float<CR>', { noremap = true })
+  end,
+}
