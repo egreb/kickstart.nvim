4e78db34d867df413f28cf16077b58d2825b090e
Simen Berge <bergesimen@gmail.com>
2025-01-25 08:37:34 +0100

    dim unfocused window

---
+++ lua/egreb/plugins/vimade.lua
@@ -0,0 +1,10 @@
+return {
+  'tadaa/vimade',
+  -- default opts (you can partially set these or configure them however you like)
+  opts = {
+    -- Recipe can be any of 'default', 'minimalist', 'duo', and 'ripple'
+    -- Set animate = true to enable animations on any recipe.
+    -- See the docs for other config options.
+    recipe = { 'paradox', { animate = false } },
+  },
+}
