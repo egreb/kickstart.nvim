12376c69fd824353d2fbce04f2067c061f04615f
Simen Berge <bergesimen@gmail.com>
2025-01-25 08:37:51 +0100

    auto preview docs as typing

--- lua/egreb/plugins/blink.lua
+++ lua/egreb/plugins/blink.lua
@@ -9,9 +9,19 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono',
       },
-
-      -- experimental signature help support
-      signature = { enabled = true },
+      -- https://cmp.saghen.dev/configuration/completion.html
+      completion = {
+        menu = { border = 'single', auto_show = true },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          window = { border = 'single' },
+        },
+        trigger = {
+          show_on_keyword = true,
+        },
+      },
+      signature = { window = { border = 'single' } },
     },
     -- allows extending the providers array elsewhere in your config
     -- without having to redefine it
