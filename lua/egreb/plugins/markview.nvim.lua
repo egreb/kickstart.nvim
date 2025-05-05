c472173add11566549705c23e06d617fb378428c
Simen Berge <bergesimen@gmail.com>
2025-05-05 09:17:10 +0200

    updte markdown rendering

--- lua/egreb/plugins/markview.nvim.lua
+++ lua/egreb/plugins/markview.nvim.lua
@@ -1,6 +1,7 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
+  enabled = false,
   ft = { 'markdown', 'markdown-inline' },
   config = function()
     local preset = require('markview.presets').headings
