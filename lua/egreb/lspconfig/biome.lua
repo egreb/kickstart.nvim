e996283718775e9f23bca3f470fcf17df3737705
Simen Berge <bergesimen@gmail.com>
2025-05-20 07:29:44 +0200

    update biome config to not load if no config present

--- lua/egreb/lspconfig/biome.lua
+++ lua/egreb/lspconfig/biome.lua
@@ -1,6 +1,13 @@
 return {
   cmd = { 'biome', 'lsp-proxy' },
-  root_dir = function(...)
-    return require('lspconfig.util').root_pattern '.git' (...)
+  root_dir = function(fname)
+    local util = require 'lspconfig.util'
+    local has_biome = util.root_pattern('biome.json', 'biome.jsonc')(fname)
+
+    if has_biome then
+      return util.root_pattern '.git'(fname)
+    end
+
+    return nil -- prevent LSP from starting
   end,
 }
