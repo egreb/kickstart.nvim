8ca0bd94857732d652ebdedc9a6057c34284f1d9
Simen Berge <bergesimen@gmail.com>
2025-08-24 07:57:52 +0200

    save before rewrite

--- lua/egreb/plugins/colorscheme.lua
+++ lua/egreb/plugins/colorscheme.lua
@@ -1,37 +1,36 @@
--- return {
---   {
---     'olimorris/onedarkpro.nvim',
---     priority = 1000,
---     config = function()
---       require('onedarkpro').setup {
---         options = {
---           highlight_inactive_windows = false,
---         },
---       }
---     end,
---     dependencies = {
---       {
---         'afonsofrancof/OSC11.nvim',
---         opts = {
---           -- Function to call when switching to dark theme
---           on_dark = function()
---             vim.opt.background = 'dark'
---             vim.cmd 'colorscheme onedark'
---           end,
---           -- Function to call when switching to light theme
---           on_light = function()
---             vim.opt.background = 'light'
---             vim.cmd 'colorscheme onelight'
---           end,
---         },
---       },
---     },
---   },
--- }
 return {
-  'fenetikm/falcon',
-  priority = 1000,
-  config = function()
-    vim.cmd 'colorscheme falcon'
-  end,
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    config = function()
+      require('tokyonight').setup {
+        style = 'night',
+        dim_inactive = true,
+      }
+    end,
+    dependencies = {
+      {
+        'afonsofrancof/OSC11.nvim',
+        opts = {
+          -- Function to call when switching to dark theme
+          on_dark = function()
+            vim.opt.background = 'dark'
+            vim.cmd 'colorscheme tokyonight-night'
+          end,
+          -- Function to call when switching to light theme
+          on_light = function()
+            vim.opt.background = 'light'
+            vim.cmd 'colorscheme tokyonight-day'
+          end,
+        },
+      },
+    },
+  },
 }
+-- return {
+--   'fenetikm/falcon',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd 'colorscheme falcon'
+--   end,
+-- }
