afddc011dac46de8a3bce567254d634a0b622538
Simen Berge <bergesimen@gmail.com>
2025-10-23 22:15:48 +0200

    add mini.jump

--- lua/egreb/plugins/mini.nvim.lua
+++ lua/egreb/plugins/mini.nvim.lua
@@ -9,18 +9,19 @@ return { -- Collection of various small independent plugins/modules
 		--  - va)  - [V]isually select [A]round [)]paren
 		--  - yinq - [Y]ank [I]nside [N]ext [']quote
 		--  - ci'  - [C]hange [I]nside [']quote
-		require('mini.ai').setup()
+		require('mini.ai').setup {}
 
 		-- Add/delete/replace surroundings (brackets, quotes, etc.)
 		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 		-- - sd'   - [S]urround [D]elete [']quotes
 		-- - sr)'  - [S]urround [R]eplace [)] [']
-		require('mini.surround').setup()
-		require('mini.indentscope').setup()
-		require('mini.move').setup()
-		require('mini.pick').setup()
+		require('mini.surround').setup {}
+		require('mini.indentscope').setup {}
+		require('mini.move').setup {}
+		require('mini.pick').setup {}
 		require('mini.pairs').setup {}
 		require('mini.extra').setup {}
+		require('mini.jump').setup {}
 
 		local use_cterm, palette
 
